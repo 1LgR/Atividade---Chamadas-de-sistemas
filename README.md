05dbf8ce3f7cd9d4b1e9e78baac35c4bb7d050e4
Luiz Gustavo Oliveira Rocha <130007640+1LgR@users.noreply.github.com>
2024-03-24 19:19:26 +0000

    README modificado e uptime finalizado

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Atividade-Chamadas-de-sistemas
 Atividade da disciplina de sistemas operacionais.
+
+## Date
+O comando date depende de chamadas de sistema para obter a hora atual. Normalmente, ele faz uso da função gettimeofday() ou clock_gettime(), que são chamadas de sistema responsáveis por obter o tempo atual do sistema. Essas chamadas de sistema acessam um contador de tempo de hardware mantido pelo kernel do Linux.
+
+A função que eu utilizo é a clock_gettime(), ela permite que sistema acesse um relógio específico, como o relógio de tempo real (CLOCK_REALTIME), que fornece o tempo do sistema em relação ao Unix Epoch (00:00:00 UTC em 1 de janeiro de 1970), com precisão em nanossegundos. 
+
+## Uptime
+
+A chamada de sistema uptime é utilizada para obter informações sobre o tempo de atividade do sistema operacional. Quando você executa o comando uptime no kernel do linux, ele mostra o tempo que o sistema está funcionando, bem como outras informações relevantes, como o número de usuários logados e a carga média do sistema nos últimos 1, 5 e 15 minutos.
+
+Internamente, a chamada de sistema uptime obtém essas informações consultando o arquivo /proc/uptime. Este arquivo contém dois números separados por um espaço em branco: o primeiro número representa o tempo em segundos desde que o sistema foi inicializado, e o segundo número representa o tempo que o sistema passou em estado ocioso.
--- uptime.c
+++ uptime.c
@@ -1,14 +1,40 @@
-#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/sysinfo.h>
+#include <time.h>
 
 #define BUF_SIZE 128
 
 int main() {
-    int fd;  // Descritor de arquivo
-    char buf[BUF_SIZE];  // Buffer para armazenar os dados lidos
-    double uptime_seconds;  // Variável para armazenar o tempo de atividade em segundos
+
+    //Utilizando o codigo do date para adquirir o horário atual
+    struct timespec current_time;
+
+    clock_gettime(CLOCK_REALTIME, &current_time);
+
+    time_t atual = current_time.tv_sec;
+    struct tm *tm_info;
+    tm_info = localtime(&atual);
+
+    //Estrutura para adquirir informações do sistema
+    struct sysinfo info;
+
+    // Obtém as informações do sistema
+    if (sysinfo(&info) != 0) {
+        perror("Erro ao obter informações do sistema");
+        exit(EXIT_FAILURE);
+    }
+    /*Criação de variaveis de armazenamento e uma matriz, a fd é uma variavel 
+    que será usada para armazenar o descritor de arquivo retornado pela função 
+    open, a buf é uma matriz para armazenar os dados lidos do arquivo, e variavel 
+    uptime  será usada para armazenar o tempo de atividade do sistema lido do arquivo 
+    /proc/uptime*/
+    int fd;
+    char buf[BUF_SIZE];
+    double uptime_segundos;
 
     // Abre o arquivo /proc/uptime para leitura
     fd = open("/proc/uptime", O_RDONLY);
@@ -17,26 +43,35 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    // Lê o conteúdo do arquivo /proc/uptime para obter o tempo de atividade em segundos
-    ssize_t bytes_read = read(fd, buf, BUF_SIZE);
-    if (bytes_read == -1) {
-        perror("Erro ao ler o arquivo /proc/uptime");
+    // Lê os valores de tempo total de atividade e tempo de ociosidade do sistema
+    ssize_t ler_bytes = read(fd, buf, BUF_SIZE);
+    if (ler_bytes == -1) {
+        perror("Erro ao ler os valores de tempo de atividade e tempo ocioso");
         close(fd);
         exit(EXIT_FAILURE);
     }
 
-    // Fecha o arquivo após a leitura do tempo de atividade
+    // Fecha o arquivo após a leitura dos valores
     close(fd);
 
-    // Converte os valores lidos para o tempo de atividade em segundos
-    uptime_seconds = atof(buf);
+    // Extrai os valores de tempo de atividade do sistema
+    char *token = strtok(buf, " ");
+    if (token == NULL) {
+        perror("Erro ao extrair os valores de tempo de atividade e tempo ocioso");
+        exit(EXIT_FAILURE);
+    }
+    uptime_segundos = atof(token);
 
     // Calcula o tempo de atividade em horas, minutos e segundos
-    int hours = uptime_seconds / 3600;
-    int minutes = ((int)uptime_seconds % 3600) / 60;
+    int horas = (int)uptime_segundos / 3600;
+    int minutos = ((int)uptime_segundos % 3600) / 60;
 
     // Imprime o tempo de atividade no formato padrão do comando uptime
-    printf("%02d:%02d:00 up %d minutes\n", hours, minutes, hours * 60 + minutes);
+    printf(" %02d:%02d:%02d up %2d:%02d,  0 users,  load average: %.2f, %.2f, %.2f\n", 
+           tm_info->tm_hour, tm_info->tm_min, tm_info->tm_sec, horas, minutos, 
+           info.loads[0] / (float)(1 << SI_LOAD_SHIFT),
+           info.loads[1] / (float)(1 << SI_LOAD_SHIFT),
+           info.loads[2] / (float)(1 << SI_LOAD_SHIFT));
 
     return 0;
 }
