5253ae2b1e74e238e51b8a1d3857e304605c3b7f
Luiz Gustavo Oliveira Rocha <130007640+1LgR@users.noreply.github.com>
2024-03-24 23:14:02 +0000

    modificações no codigo de mv

--- mv.c
+++ mv.c
@@ -2,17 +2,18 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    // Verifica se o número de argumentos está correto
+    //Verifica se o número de argumentos está correto
     if (argc != 3) {
         fprintf(stderr, "Uso: %s <arquivo_origem> <arquivo_destino>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    // Obtém os nomes dos arquivos de origem e destino
+    //Obtém os nomes dos arquivos de origem e destino
     char *arquivo_origem = argv[1];
     char *arquivo_destino = argv[2];
 
-    // Renomeia o arquivo de origem para o nome do arquivo de destino
+    /*Renomeia o arquivo de origem para o nome do arquivo de destino, utilizando a
+    chamada de sistema rename*/
     int resultado = rename(arquivo_origem, arquivo_destino);
     if (resultado == -1) {
         perror("Erro ao renomear arquivo");
